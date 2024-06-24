41506c95c66e6f326cf7dcc332d73085ea392ec0
TapiwaHerbetNosenga <144127869+TapiwaHerbetNosenga@users.noreply.github.com>
2024-06-24 21:33:33 +0200

    changed my podcast card display

--- react-app/pages/preview.jsx
+++ react-app/pages/preview.jsx
@@ -1,33 +1,12 @@
-import { useState, useEffect } from 'react';
-import { useParams } from 'react-router-dom';
+import { useState, useEffect } from "react";
+import { useParams, Link } from "react-router-dom";
 
+import Podcasts from "../components/sort";
 
-  export default function Previews() {
-
-    const [podcasts, setPodcasts] = useState([]);
-   
-  
-    useEffect(() => {
-      fetch('https://podcast-api.netlify.app')
-        .then(response => response.json())
-        .then(data => {
-          setPodcasts(data); 
-        })
-        .catch(error => console.error(error));
-    }, []); 
-  
-    return (
-       
-      <div  className='podcast-list'>
-         {podcasts.map(podcast => (
-          <Link key={podcast.id} to={`/shows/${podcast.id}`}>
-          <div key={podcast.id} className="podcast-card">
-            <img src={podcast.image} alt={podcast.title} />
-            <h2>{podcast.title}</h2>
-          </div>
-          </Link>
-        ))}
-      </div>
-     
-    );
-  }
+export default function Previews() {
+  return (
+    <>
+      <Podcasts />
+    </>
+  );
+}
