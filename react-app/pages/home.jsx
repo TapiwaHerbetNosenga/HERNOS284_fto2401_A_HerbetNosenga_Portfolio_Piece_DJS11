669f138b220d1897f09ae2ebff48e5445dec93cc
TapiwaHerbetNosenga <144127869+TapiwaHerbetNosenga@users.noreply.github.com>
2024-06-25 06:21:31 +0200

    added a page that manages the favourite episodes interface

--- react-app/pages/home.jsx
+++ react-app/pages/home.jsx
@@ -2,8 +2,8 @@ import {Link} from "react-router-dom"
 
 export default function Home() {
     return (
-        <div >
-            <h1>Home</h1>
+        <div className="border1" >
+            <h1 className="head1">Welcome to the podcast app, here you can find and listen to many different podcasts.</h1>
             
         </div>
     )
